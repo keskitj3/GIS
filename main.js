e1974d30ced4c6f41d0fb6d4f59cfea820c57f7d
keskitj3 <juhani.keskitalo@aalto.fi>
2016-06-15 12:29:20 +0300

    Update main.js

--- main.js
+++ main.js
@@ -104,7 +104,7 @@ function init() {
 	});
 
 
-	map = new ol.Map({
+/*	map = new ol.Map({
 	  renderer: "canvas",
 	  controls: ol.control.defaults({
 	    attribution: false
@@ -121,6 +121,62 @@ function init() {
 	  target: "map",
 	  view: view 
 	});
+*/
+
+var googleLayer = new olgm.layer.Google();
+
+var osmLayer = new ol.layer.Tile({
+  source: new ol.source.OSM(),
+  visible: false
+});
+
+var source = new ol.source.Vector();
+var feature = new ol.Feature(new ol.geom.Point(center));
+feature.setStyle(new ol.style.Style({
+    image: new ol.style.Circle({
+      'fill': new ol.style.Fill({color: 'rgba(153,51,51,0.3)'}),
+      'stroke': new ol.style.Stroke({color: 'rgb(153,51,51)', width: 2}),
+      'radius': 20
+    })
+  }));
+source.addFeature(feature);
+var vector = new ol.layer.Vector({
+  source: source
+});
+
+var map = new ol.Map({
+  // use OL3-Google-Maps recommended default interactions
+  interactions: olgm.interaction.defaults(),
+  controls: ol.control.defaults().extend([
+    new ol.control.ZoomSlider()
+  ]),
+  layers: [
+    googleLayer,
+    osmLayer,
+    vector
+  ],
+  target: 'map',
+  view: new ol.View({
+    center: center,
+    zoom: 12
+  })
+});
+
+var olGM = new olgm.OLGoogleMaps({map: map}); // map is the ol.Map instance
+olGM.activate();
+
+
+function toggle() {
+  googleLayer.setVisible(!googleLayer.getVisible());
+  osmLayer.setVisible(!osmLayer.getVisible());
+};
+
+
+
+
+
+
+
 
 	map.addControl(new ol.control.Zoom());
 
